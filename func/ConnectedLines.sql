b11250c87d9e0ea31f4a0e8dec9faf67778d1ca6
leendertvanwolfswinkel <leendert.vanwolfswinkel@nelen-schuurmans.nl>
2019-01-31 21:46:05 +0100

    Update ConnectedLines en nieuw: ConnectedPipeStats
    ConnectedLines werkt nu (als het goed is) voor alle Node Objects. Nog niet voor Lines.
    ConnectedPipeStats toegevoegd

--- func/ConnectedLines.sql
+++ func/ConnectedLines.sql
@@ -37,118 +37,148 @@ RETURNS
 	setof record
 AS
 $BODY$
+	DECLARE 
+		cono_ids_node_obj integer[];
 	BEGIN
-		-- checks on inputs
-		IF object_type NOT IN ('v2_connection_nodes')
-		THEN RAISE EXCEPTION '% is not a valid object_type or functionality for % has not yet been implemented', object_type, object_type;
+		-- differ behaviour for node objects, line objects and invalid objects
+		IF object_type IN ('v2_connection_nodes', 'v2_manhole', 'v2_surface', 'v2_impervious_surface', 'v2_1d_lateral', 'v2_1d_boundary_conditions')
+			THEN 
+				RAISE NOTICE 'Object type is a Node Object; ''side'' argument will be disregarded.';
+				IF 	object_type = 'v2_connection_nodes'
+					THEN 	SELECT array_agg(id) FROM v2_connection_nodes WHERE id = object_id INTO cono_ids_node_obj;
+					ELSIF 	object_type = 'v2_manhole'
+					THEN 	SELECT array_agg(id) FROM v2_connection_nodes WHERE id = (SELECT connection_node_id FROM v2_manhole WHERE id = object_id) INTO cono_ids_node_obj;
+					ELSIF	object_type = 'v2_surface'
+					THEN 	SELECT array_agg(id) FROM v2_connection_nodes WHERE id = (SELECT connection_node_id FROM v2_surface_map AS suma JOIN v2_surface AS su ON su.id = suma.surface_id WHERE su.id = object_id) INTO cono_ids_node_obj;
+					ELSIF	object_type = 'v2_impervious_surface'
+					THEN 	SELECT array_agg(id) FROM v2_connection_nodes WHERE id = (SELECT connection_node_id FROM v2_impervious_surface_map AS suma JOIN v2_impervious_surface AS su ON su.id = suma.impervious_surface_id WHERE su.id = object_id) INTO cono_ids_node_obj;
+					ELSIF 	object_type = 'v2_1d_lateral'
+					THEN 	SELECT array_agg(id) FROM v2_connection_nodes WHERE id = (SELECT connection_node_id FROM v2_1d_lateral WHERE id = object_id) INTO cono_ids_node_obj;
+					ELSIF 	object_type = 'v2_1d_boundary_conditions'
+					THEN 	SELECT array_agg(id) FROM v2_connection_nodes WHERE id = (SELECT connection_node_id FROM v2_1d_boundary_conditions WHERE id = object_id) INTO cono_ids_node_obj;
+				END IF;
+				
+				RETURN QUERY 
+				SELECT 	NULL::varchar(5) AS input_side, 
+						(_ConnectedLinesForNode(unnest(cono_ids_node_obj))).*
+				;
+
+					
+			ELSIF object_type IN ('v2_channel', 'v2_culvert', 'v2_weir', 'v2_orifice', 'v2_pumpstation', 'v2_pipe')
+			THEN RAISE EXCEPTION 'ConnectedLines for object_type % not yet implemented', object_type;
+			ELSE RAISE EXCEPTION '% is not a valid object_type', object_type;
 		END IF;
 		
 		IF side NOT IN ('start'::varchar(5), 'end'::varchar(5), 'both'::varchar(5)) 
-		THEN RAISE EXCEPTION '% is not a valid string for side argument', side USING HINT = 'Choose from start, end or both.';
+			THEN RAISE EXCEPTION '% is not a valid string for side argument', side USING HINT = 'Choose from ''start'', ''end'' or ''both''.';
 		END IF;
 		
-		IF object_type = 'v2_connection_nodes'
-		THEN
-		RETURN QUERY
-			-- pumpstation
-			SELECT 	NULL::varchar(5) AS input_side, 
-					'v2_pumpstation'::varchar(64) AS connected_object_type, 
-					connection_node_start_id AS connected_object_id, 
-					'start'::varchar(5)::varchar(5) AS connected_object_side 
-			FROM 	v2_pumpstation 
-			WHERE 	connection_node_start_id = object_id
-			UNION
-			SELECT 	NULL::varchar(5) AS input_side, 
-					'v2_pumpstation'::varchar(64) AS connected_object_type, 
-					connection_node_start_id AS connected_object_id, 
-					'end'::varchar(5) AS connected_object_side 
-			FROM 	v2_pumpstation 
-			WHERE 	connection_node_end_id = object_id
-			UNION
-			
-			-- pipe
-			SELECT 	NULL::varchar(5) AS input_side, 
-					'v2_pipe'::varchar(64) AS connected_object_type, 
-					connection_node_start_id AS connected_object_id, 
-					'start'::varchar(5) AS connected_object_side 
-			FROM 	v2_pipe 
-			WHERE 	connection_node_start_id = object_id
-			UNION
-			SELECT 	NULL::varchar(5) AS input_side, 
-					'v2_pipe'::varchar(64) AS connected_object_type, 
-					connection_node_start_id AS connected_object_id, 
-					'end'::varchar(5) AS connected_object_side 
-			FROM 	v2_pipe 
-			WHERE 	connection_node_end_id = object_id
-			UNION
-			
-			-- weir
-			SELECT 	NULL::varchar(5) AS input_side, 
-					'v2_weir'::varchar(64) AS connected_object_type, 
-					connection_node_start_id AS connected_object_id, 
-					'start'::varchar(5) AS connected_object_side 
-			FROM 	v2_weir 
-			WHERE 	connection_node_start_id = object_id
-			UNION
-			SELECT 	NULL::varchar(5) AS input_side, 
-					'v2_weir'::varchar(64) AS connected_object_type, 
-					connection_node_start_id AS connected_object_id, 
-					'end'::varchar(5) AS connected_object_side 
-			FROM 	v2_weir 
-			WHERE 	connection_node_end_id = object_id
-			UNION
-			
-			-- orifice
-			SELECT 	NULL::varchar(5) AS input_side, 
-					'v2_orifice'::varchar(64) AS connected_object_type, 
-					connection_node_start_id AS connected_object_id, 
-					'start'::varchar(5) AS connected_object_side 
-			FROM 	v2_orifice 
-			WHERE 	connection_node_start_id = object_id
-			UNION
-			SELECT 	NULL::varchar(5) AS input_side, 
-					'v2_orifice'::varchar(64) AS connected_object_type, 
-					connection_node_start_id AS connected_object_id, 
-					'end'::varchar(5) AS connected_object_side 
-			FROM 	v2_orifice 
-			WHERE 	connection_node_end_id = object_id
-			UNION
-			
-			-- culvert
-			SELECT 	NULL::varchar(5) AS input_side, 
-					'v2_culvert'::varchar(64) AS connected_object_type, 
-					connection_node_start_id AS connected_object_id, 
-					'start'::varchar(5) AS connected_object_side 
-			FROM 	v2_culvert 
-			WHERE 	connection_node_start_id = object_id
-			UNION
-			SELECT 	NULL::varchar(5) AS input_side, 
-					'v2_culvert'::varchar(64) AS connected_object_type, 
-					connection_node_start_id AS connected_object_id, 
-					'end'::varchar(5) AS connected_object_side 
-			FROM 	v2_culvert 
-			WHERE 	connection_node_end_id = object_id
-			UNION
-			
-			-- channel
-			SELECT 	NULL::varchar(5) AS input_side, 
-					'v2_channel'::varchar(64) AS connected_object_type, 
-					connection_node_start_id AS connected_object_id, 
-					'start'::varchar(5) AS connected_object_side 
-			FROM 	v2_channel 
-			WHERE 	connection_node_start_id = object_id
-			UNION
-			SELECT 	NULL::varchar(5) AS input_side, 
-					'v2_channel'::varchar(64) AS connected_object_type, 
-					connection_node_start_id AS connected_object_id, 
-					'end'::varchar(5)  AS connected_object_side 
-			FROM 	v2_channel 
-			WHERE 	connection_node_end_id = object_id
-			;
-		END IF;
 	END;
 $BODY$ LANGUAGE plpgsql;
 
+CREATE OR REPLACE FUNCTION _ConnectedLinesForNode (
+	connection_node_id integer,
+	connected_object_type OUT varchar(64), 
+	connected_object_id OUT integer, 
+	connected_object_side OUT varchar(5)
+)
+RETURNS
+	setof record
+AS
+$BODY$
+	BEGIN
+		-- checks on inputs
+		RETURN QUERY
+			-- pumpstation
+			SELECT 	'v2_pumpstation'::varchar(64) AS connected_object_type, 
+					id AS connected_object_id, 
+					'start'::varchar(5)::varchar(5) AS connected_object_side 
+			FROM 	v2_pumpstation 
+			WHERE 	connection_node_start_id = connection_node_id
+			UNION
+			SELECT 	'v2_pumpstation'::varchar(64) AS connected_object_type, 
+					id AS connected_object_id, 
+					'end'::varchar(5) AS connected_object_side 
+			FROM 	v2_pumpstation 
+			WHERE 	connection_node_end_id = connection_node_id
+			UNION
+			
+			-- pipe
+			SELECT 	'v2_pipe'::varchar(64) AS connected_object_type, 
+					id AS connected_object_id, 
+					'start'::varchar(5) AS connected_object_side 
+			FROM 	v2_pipe 
+			WHERE 	connection_node_start_id = connection_node_id
+			UNION
+			SELECT 	'v2_pipe'::varchar(64) AS connected_object_type, 
+					id AS connected_object_id, 
+					'end'::varchar(5) AS connected_object_side 
+			FROM 	v2_pipe 
+			WHERE 	connection_node_end_id = connection_node_id
+			UNION
+			
+			-- weir
+			SELECT 	'v2_weir'::varchar(64) AS connected_object_type, 
+					id AS connected_object_id, 
+					'start'::varchar(5) AS connected_object_side 
+			FROM 	v2_weir 
+			WHERE 	connection_node_start_id = connection_node_id
+			UNION
+			SELECT 	'v2_weir'::varchar(64) AS connected_object_type, 
+					id AS connected_object_id, 
+					'end'::varchar(5) AS connected_object_side 
+			FROM 	v2_weir 
+			WHERE 	connection_node_end_id = connection_node_id
+			UNION
+			
+			-- orifice
+			SELECT 	'v2_orifice'::varchar(64) AS connected_object_type, 
+					id AS connected_object_id, 
+					'start'::varchar(5) AS connected_object_side 
+			FROM 	v2_orifice 
+			WHERE 	connection_node_start_id = connection_node_id
+			UNION
+			SELECT 	'v2_orifice'::varchar(64) AS connected_object_type, 
+					id AS connected_object_id, 
+					'end'::varchar(5) AS connected_object_side 
+			FROM 	v2_orifice 
+			WHERE 	connection_node_end_id = connection_node_id
+			UNION
+			
+			-- culvert
+			SELECT 	'v2_culvert'::varchar(64) AS connected_object_type, 
+					id AS connected_object_id, 
+					'start'::varchar(5) AS connected_object_side 
+			FROM 	v2_culvert 
+			WHERE 	connection_node_start_id = connection_node_id
+			UNION
+			SELECT 	'v2_culvert'::varchar(64) AS connected_object_type, 
+					id AS connected_object_id, 
+					'end'::varchar(5) AS connected_object_side 
+			FROM 	v2_culvert 
+			WHERE 	connection_node_end_id = connection_node_id
+			UNION
+			
+			-- channel
+			SELECT 	'v2_channel'::varchar(64) AS connected_object_type, 
+					id AS connected_object_id, 
+					'start'::varchar(5) AS connected_object_side 
+			FROM 	v2_channel 
+			WHERE 	connection_node_start_id = connection_node_id
+			UNION
+			SELECT 	'v2_channel'::varchar(64) AS connected_object_type, 
+					id AS connected_object_id, 
+					'end'::varchar(5)  AS connected_object_side 
+			FROM 	v2_channel 
+			WHERE 	connection_node_end_id = connection_node_id
+		;
+	END;
+$BODY$ LANGUAGE plpgsql;
 
-
-
+/* Testen
+  
+  SELECT (ConnectedLines('v2_manhole', 4374)).*
+                                                                                
+  SELECT id FROM v2_manhole ORDER BY id LIMIT 100;
+                                                                                
+*/
---
+++ func/ConnectedPipeStats.sql
@@ -0,0 +1,92 @@
+/*
+
+DESCRIPTION: 
+Returns statistics on the pipes connected to a manhole
+
+INPUTS:
+- manhole_id integer
+- [OPTIONAL] <Optional input arguments + their description>
+
+OUTPUTS: 
+- <What is returned by the function>
+- <How is the database affected / what edits in which tables result from calling the function>
+
+
+DEPENDENCIES:
+- What functions does this function depend on that are not available in postgresql, postgis or pg3Di ?
+
+REMARKS: 
+- Anything the user should be aware of 
+
+EXAMPLE(S):
+- -- Get stats for all manholes in model:
+  SELECT id AS manhole_id, (ConnectedPipeStats (id)).* FROM v2_manhole;
+  
+*/
+
+DROP FUNCTION IF EXISTS ConnectedPipeStats(integer);
+CREATE OR REPLACE FUNCTION ConnectedPipeStats (
+	manhole_id integer,
+	count_total OUT integer,
+	count_mixed OUT integer,
+	count_rain_water OUT integer,
+	count_dry_weather OUT integer,
+	count_transport OUT integer,
+	count_spillway OUT integer,
+	count_syphon OUT integer,
+	count_storage OUT integer,
+	count_storage_tank OUT integer,
+	invert_level_min  OUT double precision,
+	invert_level_avg  OUT double precision,
+	invert_level_max  OUT double precision
+)	
+RETURNS
+	record
+AS
+$BODY$
+    WITH conn_lines AS (
+        SELECT (ConnectedLines('v2_manhole', manhole_id)).*
+    ), 
+    connected_pipes AS (
+        SELECT	p.*,
+                cl.connected_object_side
+        FROM 	v2_pipe AS p
+        JOIN 	conn_lines AS cl
+            ON	p.id = cl.connected_object_id
+        WHERE 	cl.connected_object_type = 'v2_pipe'
+
+    ),
+    connected_invert_levels AS (
+        SELECT invert_level_start_point AS invert_level FROM connected_pipes WHERE connected_object_side = 'start' 
+        UNION
+        SELECT invert_level_end_point FROM connected_pipes WHERE connected_object_side = 'end' 
+    ), 
+    counts AS (
+        SELECT 	count(*)::integer AS count_total,
+                SUM( CASE WHEN sewerage_type = 0 THEN 1 ELSE 0 END )::integer AS count_mixed,
+                SUM( CASE WHEN sewerage_type = 1 THEN 1 ELSE 0 END )::integer AS count_rain_water,
+                SUM( CASE WHEN sewerage_type = 2 THEN 1 ELSE 0 END )::integer AS count_dry_weather,
+                SUM( CASE WHEN sewerage_type = 3 THEN 1 ELSE 0 END )::integer AS count_transport,
+                SUM( CASE WHEN sewerage_type = 4 THEN 1 ELSE 0 END )::integer AS count_spillway,
+                SUM( CASE WHEN sewerage_type = 5 THEN 1 ELSE 0 END )::integer AS count_syphon,
+                SUM( CASE WHEN sewerage_type = 6 THEN 1 ELSE 0 END )::integer AS count_storage,
+                SUM( CASE WHEN sewerage_type = 7 THEN 1 ELSE 0 END )::integer AS count_storage_tank
+        FROM 	connected_pipes
+    ),
+    invert_level_stats AS (
+        SELECT 	min(invert_level)::double precision AS invert_level_min,
+                avg(invert_level)::double precision AS invert_level_avg,
+                max(invert_level)::double precision AS invert_level_max
+        FROM 	connected_invert_levels
+    )
+    SELECT c.*, ils.* FROM counts AS c, invert_level_stats AS ils
+	;
+$BODY$ LANGUAGE sql;
+                        
+                        
+/* TESTEN
+   
+    -- Get stats for all manholes in model
+  SELECT id AS manhole_id, (ConnectedPipeStats (id)).* FROM v2_manhole;
+  
+*/
